5948944e8e7031570b24b00f572b06ca7510c6d4
Thomas <tfnielson.se@gmail.com>
2022-12-18 15:35:03 -0700

    commit tree 9a9b555a96e08dc7db344b2e20b6b47921370198

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (drivers, name) {
+    return drivers.filter(function(match) {
+        return match.toLowerCase() === name.toLowerCase()
+    });
+}
+
+console.log(findMatching(drivers, 'bobby'))
+
+function fuzzyMatch (drivers, letters) {
+    return drivers.filter((match) => match.toLowerCase().indexOf(letters.toLowerCase()) === 0 
+    );
+}
+console.log(fuzzyMatch(drivers, 'bob'));
+
+function matchName (drivers, name) {
+    return drivers.filter( function(match) {
+        return match.name === name
+    });
+    }
+
+console.log(matchName(drivers, 'Bobby'))
